c83f70b02a4dad6250c8a30bfde628aba6ed35f6
jaejae-yoo <wotj102@gmail.com>
2022-09-03 23:35:27 +0900

    project setting

---
+++ src/main/java/com/devsume/devsume/DevsumeApplication.java
@@ -0,0 +1,13 @@
+package com.devsume.devsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsumeApplication.class, args);
+    }
+
+}
